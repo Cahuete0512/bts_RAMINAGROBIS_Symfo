df87624b3b9602a54583b0a8f035a5d5d052270c
Cahuete0512 <74617561+Cahuete0512@users.noreply.github.com>
2022-03-23 16:24:45 +0100

    communication entre l api swagger et la page symfony établie (utilisation de http_client)

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Service\ApiService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,8 +10,10 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'home')]
-    public function index(): Response
+    public function index(ApiService $apiService): Response
     {
+        dd($apiService->getApiData());
+
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
